dc538eed1755e830c80eca94cd90ff99cc2d6ed8
dmxiaoshen <dmxiaoshen@163.com>
2015-08-10 23:13:47 +0800

    merge

--- blog-web/src/main/java/com/dmxiaoshen/blog/web/article/web/ArticleController.java
+++ blog-web/src/main/java/com/dmxiaoshen/blog/web/article/web/ArticleController.java
@@ -50,9 +50,15 @@ public class ArticleController {
 		//logger.debug(""+html.contains("<p>&lt;!--more--&gt;</p>"));
 		int start = html.indexOf("&lt;");
 		int end = html.indexOf("&gt;");
+		StringBuilder sb = new StringBuilder();
+		String s1 = html.substring(0,start);
+		String s2 = html.substring(end+4, html.length());
+		sb.append(html.substring(0, start)).append(html.substring(end, html.length()));
+		String kkk = sb.toString();
 		logger.debug(start+"-"+end);
 		html.replaceAll("&lt;","");
 		logger.debug(html);
+		logger.debug(kkk);
 		return "success";
 	}
 }
